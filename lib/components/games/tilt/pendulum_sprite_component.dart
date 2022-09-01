cc93159d1df11af0e4e5dc28cbae99cf376827e6
Mitchell John Couturier <couturmi@mail.gvsu.edu>
2022-09-01 09:48:04 -0400

    added arm to tilt

--- lib/components/games/tilt/pendulum_sprite_component.dart
+++ lib/components/games/tilt/pendulum_sprite_component.dart
@@ -9,15 +9,21 @@ class PendulumSpriteComponent extends PositionComponent {
       : super(anchor: Anchor.bottomCenter);
 
   Future<void> onLoad() async {
-    await add(RectangleComponent(
-      size: Vector2(10, hitCircleCenterHeight),
-      anchor: Anchor.bottomCenter,
-    ));
     await add(SpriteComponent(
+      priority: 1,
       sprite: await Sprite.load("boxing_glove.png"),
       size: Vector2.all(hitCircleDiameter),
       anchor: Anchor.center,
       position: Vector2(0, -hitCircleCenterHeight),
+    ));
+    await add(SpriteComponent(
+      priority: 0,
+      sprite: await Sprite.load("arm.png"),
+      size: Vector2(
+          hitCircleCenterHeight * 0.514, // keep aspect ratio of png.
+          hitCircleCenterHeight),
+      anchor: Anchor.bottomCenter,
+      position: Vector2(0, (hitCircleDiameter / 4)),
     ));
     await super.onLoad();
   }
